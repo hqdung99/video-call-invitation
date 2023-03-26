eb239d7761d2f6ab78c566828318ba46ef616c4e
Huynh Quoc Dung <dunghq4@vingroup.net>
2023-03-26 11:56:23 +0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import { ZIM } from "zego-zim-web";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 
-function App() {
+function randomID(len) {
+  let result = "";
+  if (result) return result;
+  var chars = "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+export default function App() {
+  const [userInfo, setUserInfo] = useState({
+    userName: "",
+    userId: "",
+  });
+  const [calleeId, setCalleeId] = useState("");
+  const zeroCloudInstance = useRef(null);
+
+  async function init() {
+    const userId = randomID(5);
+    const userName = "user_" + userId;
+    setUserInfo({
+      userName,
+      userId,
+    });
+    const appID = 123; // fill your appID here
+    const serverSecret = "XXXX"; // fill your serverSecret here
+
+    const KitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      null,
+      userId,
+      userName
+    );
+
+    zeroCloudInstance.current = ZegoUIKitPrebuilt.create(KitToken);
+    // add plugin
+    zeroCloudInstance.current.addPlugins({ ZIM });
+  }
+
+  function handleSend(callType) {
+    const callee = calleeId;
+    if (!callee) {
+      alert("userID cannot be empty!!");
+      return;
+    }
+
+    // send call invitation
+    zeroCloudInstance.current
+      .sendCallInvitation({
+        callees: [{ userID: callee, userName: "user_" + callee }],
+        callType: callType,
+        timeout: 60,
+      })
+      .then((res) => {
+        console.warn(res);
+        if (res.errorInvitees.length) {
+          alert("The user dose not exist or is offline.");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>My username: <span>{userInfo.userName}</span></div>
+      <div>My userId: <span>{userInfo.userId}</span>
+      </div>
+      <input
+        type="text"
+        is="userId"
+        placeholder="callee's userID"
+        onChange={(event) => {
+          setCalleeId(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          handleSend(ZegoUIKitPrebuilt.InvitationTypeVideoCall);
+        }}
+      >
+        Video call
+      </button>
+      <button
+        onClick={() => {
+          handleSend(ZegoUIKitPrebuilt.InvitationTypeVoiceCall);
+        }}
+      >
+        Voice call
+      </button>
     </div>
   );
 }
-
-export default App;
